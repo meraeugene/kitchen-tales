88fc39da4932b1a1a3968ac42d9787655bed4253
meraeugene <villalon.andrew123@gmail.com>
2024-09-26 18:55:52 +0800

    fix dist

--- server/src/index.ts
+++ server/src/index.ts
@@ -49,7 +49,7 @@ if (process.env.NODE_ENV === "production") {
 
   // any route that is not an api will be redirected to index.html
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+    res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
