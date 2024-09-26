73900c358baf0d6369205ab85bde0e23e0418467
meraeugene <villalon.andrew123@gmail.com>
2024-09-26 19:44:08 +0800

    fix location

--- server/src/index.ts
+++ server/src/index.ts
@@ -53,11 +53,11 @@ app.use("/api/upload", uploadImageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   // set static folder
-  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.use(express.static(path.join(__dirname, "../../client/dist")));
 
   // any route that is not an api will be redirected to index.html
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
