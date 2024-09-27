7ebdfb7b240dbf4bfe8ca6897b57ce33fb7e7eee
meraeugene <villalon.andrew123@gmail.com>
2024-09-27 15:48:55 +0800

    change website url

--- client/src/contants.ts
+++ client/src/contants.ts
@@ -1,8 +1,8 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === "development" ? "http:/localhost:5000" : "";
-// export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "http://localhost:5000";
 // export const BASE_URL = "https://kitchen-tales.onrender.com";
-export const BASE_URL = "";
+// export const BASE_URL = "";
 export const RECIPES_URL = "/api/recipes";
 export const BOOKMARKS_URL = "/api/bookmarks";
 export const ARTICLES_URL = "/api/articles";
